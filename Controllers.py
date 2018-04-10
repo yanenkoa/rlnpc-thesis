81caa7bb6fd92249ea6b74439aa7d766af6d24ca
Alexandr Yanenko <yanenkoalexandr@gmail.com>
2018-04-10 15:00:28 +0300

    'RLController' (unused right now) and refactoring

--- Controllers.py
+++ Controllers.py
@@ -1,9 +1,12 @@
-from time import time
+from collections import namedtuple
+from time import time, sleep
 
+import tensorflow as tf
 from pynput.keyboard import Key
 
 from GameObjects import World, Player, PlayerMovementDirection
 from InputDevice import InputDevice
+from RL import Learner
 from Rendering import RenderWorld
 
 
@@ -33,7 +36,7 @@ class KeyboardController:
         self._last_update_s = cur_time_s
         return elapsed_time_s
 
-    def _update_player(self, elapsed_time_s) -> None:
+    def _update_player(self, elapsed_time_s: float) -> None:
 
         if self._input_device.is_key_down("w"):
             self._world.move_player(elapsed_time_s, PlayerMovementDirection.FORWARD)
@@ -52,21 +55,67 @@ class KeyboardController:
         new_angle = self._player.angle + angle_increment
         self._world.update_player_angle(new_angle)
 
-    def start_rendering(self) -> None:
+    def _start_rendering(self) -> None:
         if self._input_device.is_key_down("v"):
             self._render_world.start_drawing()
 
-    def reset(self) -> None:
+    def _reset(self) -> None:
         if self._input_device.is_key_down("r"):
             self._world.reset()
 
     def loop(self):
         while True:
-            self.reset()
-            self.start_rendering()
+            self._reset()
+            # self._start_rendering()
+            self._render_world.start_drawing()
             elapsed_time_s = self._get_elapsed_time_s()
             self._update_player(elapsed_time_s)
             self._world.update_state(elapsed_time_s)
             self._render_world.update()
             if self._input_device.is_key_down(Key.esc):
                 break
+
+
+class RLController:
+    _world: World
+    _fps: int
+
+    _time_between_frames: float
+    _window_size: int
+    _learner: Learner
+    _render_world: RenderWorld
+    _last_update_s: float
+
+    def __init__(self, world: World, fps: int = 30):
+        self._world = world
+        self._fps = fps
+
+        self._time_between_frames = 1 / self._fps
+        self._window_size = 7
+        fake_process_config = namedtuple("SomeTuple", ["reward_discount_coef"])(0.9)
+        self._learner = Learner(world, 128, tf.Session(), self._window_size, 1. / 30,
+                                fake_process_config)
+        self._render_world = RenderWorld(self._world)
+        self._last_update_s = time()
+
+    def initialize(self) -> None:
+        self._learner.initialize()
+        self._learner.get_next_action()
+
+    def _get_elapsed_time_s(self) -> float:
+        cur_time_s = time()
+        elapsed_time_s = cur_time_s - self._last_update_s
+        self._last_update_s = cur_time_s
+        return elapsed_time_s
+
+    def loop(self) -> None:
+        i = 0
+        while True:
+            i += 1
+            print(i)
+            self._render_world.start_drawing()
+            elapsed_time_s = self._get_elapsed_time_s()
+            self._learner.apply_action(elapsed_time_s)
+            if elapsed_time_s < self._time_between_frames:
+                sleep(self._time_between_frames - elapsed_time_s)
+            self._render_world.update()
