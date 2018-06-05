326748deaf14997f7c47338f487bc43ca54fb0b1
Alexandr Yanenko <yanenkoalexandr@gmail.com>
2018-06-06 02:57:40 +0300

    'rl_config_alt_5_vc'

--- trainer/Configs.py
+++ trainer/Configs.py
@@ -677,6 +677,71 @@ def rl_config_alt_5_vc() -> Tuple[LearningProcessConfig, NetworkConfig]:
     return lp_config, net_conf
 
 
+def rl_config_alt_5_vc_eps() -> Tuple[LearningProcessConfig, NetworkConfig]:
+    frames_in_second = 60
+    n_skipped_frames = 10
+    max_minutes = 10
+    framerate = 1. / frames_in_second
+    max_ep_length = max_minutes * 60 * frames_in_second // n_skipped_frames
+    update_frequency = max_ep_length
+    lp_config = LearningProcessConfig(
+        replay_size=None,
+        update_frequency=update_frequency,
+        reward_discount_coef=0.9,
+        start_random_action_prob=1,
+        end_random_action_prob=0.05,
+        annealing_steps=2000000,
+        n_training_episodes=5000,
+        pre_train_steps=0,
+        max_ep_length=max_ep_length,
+        buffer_size=None,
+        n_skipped_frames=n_skipped_frames,
+        target_network_update_frequency=20,
+        initial_temperature=1,
+        temp_coef=1,
+        min_temperature=1,
+        framerate=framerate,
+        regularization_loss_coef=1,
+        value_loss_coef=1e-2,
+        learning_rate=0.0001,
+        clip_norm=200,
+        reset_reward_every=max_ep_length * 10,
+    )
+    net_conf = NetworkConfig(
+        window_size=7,
+        n_output_angles=16,
+        conv_configs=[
+            ConvConfig(
+                filters=16,
+                activation="prelu",
+                name="conv1",
+            ),
+            ConvConfig(
+                filters=16,
+                activation="prelu",
+                name="conv2",
+            ),
+        ],
+        lstm_configs=[
+            LSTMConfig(
+                units=400,
+                name="lstm_layer_1",
+            ),
+            LSTMConfig(
+                units=400,
+                name="lstm_layer_2",
+            ),
+            LSTMConfig(
+                units=400,
+                name="lstm_layer_3",
+            ),
+        ],
+        dense_configs=[],
+    )
+
+    return lp_config, net_conf
+
+
 def rl_config_shallower() -> Tuple[LearningProcessConfig, NetworkConfig]:
     frames_in_second = 60
     n_skipped_frames = 15
@@ -765,7 +830,7 @@ def dump_config(lpc: LearningProcessConfig, nc: NetworkConfig, path: str) -> Non
 def main():
     config_funcs = [rl_config, rl_config_shallower, rl_config_two, rl_config_larger_lr_lower_clipnorm,
                     rl_config_low_lr_low_clipnorm, rl_config_even_lower_lr_clipnorm, rl_config_alt_3,
-                    rl_config_alt_5, rl_config_alt_5_vc]
+                    rl_config_alt_5, rl_config_alt_5_vc, rl_config_alt_5_vc_eps]
     print("Choose a config to dump:")
     for i, config_func in enumerate(config_funcs):
         print("{}: {}".format(i, config_func.__name__))
